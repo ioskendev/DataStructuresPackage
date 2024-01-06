13aa44cae93e24388a231c21fc001501e8fd18bc
Dinar Khakimov <56588291+DinarKhakimov@users.noreply.github.com>
2024-01-06 16:37:53 +0500

    first test without elemets.

--- Tests/DataStructuresPackageTests/DataStructuresPackageTests.swift
+++ Tests/DataStructuresPackageTests/DataStructuresPackageTests.swift
@@ -2,6 +2,18 @@ import XCTest
 @testable import DataStructuresPackage
 
 final class DataStructuresPackageTests: XCTestCase {
+	
+	var sut: LinkedList<String>!
+	
+	override func setUp() {
+		super.setUp()
+		sut = LinkedList<String>()
+	}
+	
+	func test_empty_withoutElements_shouldBeTrue() {
+		XCTAssertTrue(sut.isEmpty)
+	}
+	
     func testExample() throws {
         // XCTest Documentation
         // https://developer.apple.com/documentation/xctest
@@ -10,68 +22,3 @@ final class DataStructuresPackageTests: XCTestCase {
         // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
     }
 }
-
-// TODO: make test with LinkedList
-
-//var list = LinkedList<Int>()
-//
-//list.push(5)
-//list.push(2)
-//
-//print(list) // 2 -> 5
-//list.append(3)
-//list.append(9)
-//
-//print(list) // 2 -> 5 -> 3 -> 9
-//list.insert(1, after: 1)
-//
-//print(list) // 2 -> 5 -> 1 -> 3 -> 9
-//list.pop()
-//
-//print(list) // 5 -> 1 -> 3 -> 9
-//list.removeLast()
-//
-//print(list) // 5 -> 1 -> 3
-//list.remove(after: 0)
-//
-//print(list) // 5 -> 3
-//list.append(4)
-//list.append(9)
-//
-//print(list) // 5 -> 3 -> 4 -> 9
-//list.remove(first: 4)
-//list.remove(after: 0)
-//
-//print(list) // 5 -> 9
-
-// TODO: to make tests with queueList
-
-//var queueList = QueueList<String>()
-//
-//queueList.enqueue("First")
-//queueList.enqueue("Second")
-//queueList.enqueue("Third")
-//queueList.enqueue("Fouth")
-//queueList.enqueue("Firth")
-//
-//queueList.isEmpty
-//
-//queueList.count
-//
-//print(queueList)
-//
-//queueList.dequeue()
-//
-//queueList.count
-//
-//print(queueList)
-//
-//queueList.dequeue()
-//
-//queueList.count
-//
-//print(queueList)
-//
-//queueList.peek()
-//
-//print(queueList)

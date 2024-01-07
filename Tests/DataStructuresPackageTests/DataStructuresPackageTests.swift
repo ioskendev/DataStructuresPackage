3a9035e65f86bd46e7f740789340c1b1000d7994
Dinar Khakimov <56588291+DinarKhakimov@users.noreply.github.com>
2024-01-07 12:19:47 +0500

    test without element, add pattern AAA

--- Tests/DataStructuresPackageTests/DataStructuresPackageTests.swift
+++ Tests/DataStructuresPackageTests/DataStructuresPackageTests.swift
@@ -2,17 +2,26 @@ import XCTest
 @testable import DataStructuresPackage
 
 final class DataStructuresPackageTests: XCTestCase {
-	
+	// Arrange
 	var sut: LinkedList<String>!
-	
+	// Act
 	override func setUp() {
 		super.setUp()
 		sut = LinkedList<String>()
 	}
-	
+	override func tearDown() {
+		super.tearDown()
+		sut = nil
+	}
+	// Assert
 	func test_empty_withoutElements_shouldBeTrue() {
 		XCTAssertTrue(sut.isEmpty)
 	}
+	func test_empty_withoutElements_shouldBeFalse(){
+		sut.push("test")
+		XCTAssertFalse(sut.isEmpty)
+	}
+	
 	
     func testExample() throws {
         // XCTest Documentation

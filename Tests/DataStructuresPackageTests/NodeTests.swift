8fc640ec5b2e64881a48a3fc218e081f1bca7569
Alexey Turulin <repakuku@icloud.com>
2024-01-07 23:20:51 -0500

    NodeTests added error messages

--- Tests/DataStructuresPackageTests/NodeTests.swift
+++ Tests/DataStructuresPackageTests/NodeTests.swift
@@ -9,26 +9,28 @@ import XCTest
 @testable import DataStructuresPackage
 
 final class NodeTests: XCTestCase {
-	
+
+	// MARK: - Init Tests
+
 	func test_init_withValue_shouldReturnNodeWithCorrectValue() {
 
 		let sut = LinkedList<Int>.Node(42)
 
-		XCTAssertEqual(sut.value, 42)
+		XCTAssertEqual(sut.value, 42, "A Node should be initialized with the correct value.")
 	}
 
 	func test_init_withValue_shouldBeNilPreviousNode() {
 
 		let sut = LinkedList<Int>.Node(42)
 
-		XCTAssertNil(sut.previous)
+		XCTAssertNil(sut.previous, "A Node initialized with only a value should have its previous node set to nil")
 	}
 
 	func test_init_withValue_shouldBeNilNextNode() {
 
 		let sut = LinkedList<Int>.Node(42)
 
-		XCTAssertNil(sut.next)
+		XCTAssertNil(sut.next, "A Node initialized with the only a value should have its next node set to nil.")
 	}
 
 	func test_init_withValueAndPreviousNode_shouldReturnPreviousNodeWithCorrectValue() {
@@ -37,7 +39,7 @@ final class NodeTests: XCTestCase {
 
 		let sut = LinkedList<Int>.Node(42, previous: previousNode)
 
-		XCTAssertEqual(sut.previous?.value, 41)
+		XCTAssertEqual(sut.previous?.value, 41, "Previous node should have the correct value.")
 	}
 
 	func test_init_withValueAndNextNode_shouldReturnNextNodeWithCorrectValue() {
@@ -46,13 +48,15 @@ final class NodeTests: XCTestCase {
 
 		let sut = LinkedList<Int>.Node(42, next: nextNode)
 
-		XCTAssertEqual(sut.next?.value, 43)
+		XCTAssertEqual(sut.next?.value, 43, "Next node should have the correct value.")
 	}
 
-	func test_description_withValue_shouldReturnStringWithCorrectvalue() {
+	// MARK: - Description Tests
+
+	func test_description_withValue_shouldReturnStringWithCorrectValue() {
 
 		let sut = LinkedList<Int>.Node(42)
 
-		XCTAssertEqual(sut.description, "42")
+		XCTAssertEqual(sut.description, "42", "The description should return a string with the correct value.")
 	}
 }
